34171800d4b217ad3cfe0cb096d3d602222088e0
Ryan Bandler <ryanbandler@gmail.com>
2022-02-20 10:26:12 -0600

    implement grid coloring logic

--- wordle/src/components/Grid.jsx
+++ wordle/src/components/Grid.jsx
@@ -1,60 +1,35 @@
-
 import { useState } from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import LetterBox from "./LetterBox";
-
 import "./Grid.css"
+
+const getStatus = (word, letter, letterIndex) => {
+  if(word[letterIndex] === letter)
+    return "correct"
+  else if(word.includes(letter))
+    return "misplaced"
+  else
+    return "wrong"
+}
+
 const Grid = () => {
 
- 
-
+  const [board, setBoard] = useState(Array(5).fill().map(() => Array(5).fill("H")));
+  const WORD = "HELLO"
   return (
     <div className="grid">
 
       <Container className="container">
-          <Row className="row">
-            <LetterBox letter="H"></LetterBox>      
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>        
-          </Row>
-          <Row className="row">
-            <LetterBox letter="H"></LetterBox>      
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>        
-          </Row>
-          <Row className="row">
-            <LetterBox letter="H"></LetterBox>      
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>        
-          </Row>
-          <Row className="row">
-            <LetterBox letter="H"></LetterBox>      
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>        
-          </Row>
-          <Row className="row">
-            <LetterBox letter="H"></LetterBox>      
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>    
-            <LetterBox letter="H"></LetterBox>        
-          </Row>
-  
-       
-       
-
-    
-       
-        
-        
+          {board.map((row, idx) => {
+            return (
+              <Row key={idx} className="row">
+                {row.map((letter, sIdx) => {
+                  const status = getStatus(WORD, row[sIdx], sIdx);
+                  return <LetterBox key={sIdx} letter={letter} status={status}/>
+                })}
+              </Row>
+            )
+          })}
       </Container>
     </div>
   )
